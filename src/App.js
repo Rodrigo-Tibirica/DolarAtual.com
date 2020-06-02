4a0f9979e5740bdaac144924a9548a2c8729bb59
unknown <rtibirica82@gmail.com>
2020-06-02 10:44:26 -0300

    Dolar Site v 0.8.1

--- src/App.js
+++ src/App.js
@@ -3,17 +3,14 @@ import './App.css';
 import './backend/buscar';
 import './teste2';
 
-const funcao = require("./backend/buscar");
-const funcao2 = `${funcao}`;
-
 function App() {
   return (
     <div className="App"> 
 
         
       <div className = "Fonte"id = "1" >
-     
-        <h1 class = "dolar" > funcao2.USD.ask </h1>
+      
+        <h1 class = "dolar" >  </h1>
 
 
        </div>
--- src/test.js
+++ src/test.js
@@ -1,11 +1,10 @@
-const buscar = require("./backend/buscar");
+//const buscar = require("./backend/buscar");
           
-    buscar().then((dolar) => {
-        const html = dolar.USD.ask;
-        var myJSON = JSON.stringify(dolar);
+    //buscar().then((dolar) => {
+       // const html = dolar.USD.ask;
+        //var myJSON = JSON.stringify(dolar);
         //console.log(typeof(myJSON));
-        document.getElementById("1").innerHTML = html;
+        //document.getElementById("1").innerHTML = html;
  
 
-    })
-w
+   // })

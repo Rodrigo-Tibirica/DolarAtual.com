263dfb18750846368522a29e2d31b280bc886dad
eduardo-diniz <eduardogomesdiniz42@gmail.com>
2020-06-06 10:38:00 -0300

    Atualizações visuais

--- src/components/template/Main.jsx
+++ src/components/template/Main.jsx
@@ -4,5 +4,5 @@ import "../../styles/css/Main.css";
 import Dolar from "../Pages/Dolar/Dolar"
 import Euro from "../Pages/Euro/Euro"
 
-//export default (props) => <div className="main"><Dolar/></div>;
-export default (props) => <div className="main"><Euro/></div>;
+export default (props) => <div className="main"><Dolar/></div>;
+//export default (props) => <div className="main"><Euro/></div>;

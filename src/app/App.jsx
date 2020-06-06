94bf7a628e960c330e996902b134415ecd839210
Fábio Barros Ribeiro <whatupfabio@gmail.com>
2020-06-06 19:35:09 -0300

    Usando React Hooks

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,9 +7,11 @@ import SideBarLeft from "../components/template/SideBarLeft";
 import SideBarRight from "../components/template/SideBarRight";
 import Main from "../components/template/Main";
 import Footer from "../components/template/Footer";
+import {BrowserRouter } from "react-router-dom";
 
 export default (props) => {
     return (
+        <BrowserRouter> 
         <div className="app">
             <Logo />
             <Header />
@@ -18,5 +20,6 @@ export default (props) => {
             <SideBarRight/>
             <Footer />
         </div>
+        </BrowserRouter>
     );
 };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,18 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router";
 
 import Dolar from "../components/pages/dolar/Dolar";
 import Euro from "../components/pages/euro/Euro";
 
 export default function Routes() {
     return (
-        <BrowserRouter>
-            <Switch>
-                <Route path="/" exact component={Dolar}></Route>
-                <Route path="/dolaratual" exact component={Dolar}></Route>
-                <Route path="/euroatual" exact component={Euro}></Route>
-            </Switch>
-        </BrowserRouter>
+        
+        <Switch>
+            <Route path="/" exact component={Dolar}></Route>
+            <Route path="/dolaratual" exact component={Dolar}></Route>
+            <Route path="/euroatual" exact component={Euro}></Route>
+            <Redirect from="*" to="/" />
+        </Switch>
     );
 }
 

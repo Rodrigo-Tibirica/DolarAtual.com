763d87804853a741580e5288ccc80c4900165129
eduardo-diniz <eduardogomesdiniz42@gmail.com>
2020-06-06 19:38:50 -0300

    Teste

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,18 +1,15 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 
-import Dolar from "../components/pages/dolar/Dolar";
-import Euro from "../components/pages/euro/Euro";
+import Dolar from "../components/Pages/dolar/Dolar";
+import Euro from "../components/Pages/euro/Euro";
 
 export default function Routes() {
     return (
-        <BrowserRouter>
             <Switch>
-                <Route path="/" exact component={Dolar}></Route>
                 <Route path="/dolaratual" exact component={Dolar}></Route>
                 <Route path="/euroatual" exact component={Euro}></Route>
             </Switch>
-        </BrowserRouter>
     );
 }
 

feb842c5f19a5feb219925d0c67ccbcc40aad3b4
eduardo-diniz <eduardogomesdiniz42@gmail.com>
2020-06-06 15:16:06 -0300

    Remoção Css duplicado

--- src/components/template/Header.jsx
+++ src/components/template/Header.jsx
@@ -1,15 +1,9 @@
 import React from "react";
 import "../../styles/css/Header.css";
-<<<<<<< HEAD
+
 import Router from '../../routes/routes'
 import Dolar from "../Pages/Dolar/Dolar"
 import Euro from "../Pages/Euro/Euro"
-=======
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Switch from '@material-ui/core/Switch';
->>>>>>> parent of 0dc75ca... Remoção DarkMode
-
-import Button from '@material-ui/core/Button';
 
 export default (props) => {
     return (
@@ -28,14 +22,12 @@ export default (props) => {
                 </ul>
             </nav>
 
-<<<<<<< HEAD
-        </header>
-=======
-            <div className="DarkMode">
-                <FormControlLabel control={<Switch />} label="Darkmode" />
-                </div>
 
-        </header >
->>>>>>> parent of 0dc75ca... Remoção DarkMode
+
+        </header>
+
+
+
+      
     );
 };
--- src/components/template/SideBarRight.jsx
+++ src/components/template/SideBarRight.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import "../../styles/css/SideBarRight.css";
 export default (props) => {
-    return(<aside className="right">Nav</aside>)
+    return(<aside className="right"></aside>)
 }

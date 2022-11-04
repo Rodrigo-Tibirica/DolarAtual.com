9db34272fbcc7ddca36a63ed16037a9a7b845344
Eduardo Diniz <48698163+eduardo-diniz@users.noreply.github.com>
2022-11-03 23:22:48 -0300

    Update Dolar.jsx

--- src/components/Pages/Dolar/Dolar.jsx
+++ src/components/Pages/Dolar/Dolar.jsx
@@ -21,9 +21,7 @@ export default (params) => {
         })
             .then((response) => {
                 return response.data;
-                // const valor = parseFloat(response.data.USD.ask).toFixed(2);
-                // const data = response.data.USD.create_date;
-                // this.setState({ valor, data });
+                
             })
             .catch((error) => {
                 console.log(error);
@@ -34,10 +32,10 @@ export default (params) => {
         const aux = split[0].split("-");
         const data_formatada = aux[2] + "/" + aux[1] + "/" + aux[0];
         const hora = split[1];
+        
         setValor(cambio);
         setData(data_formatada);
         setHora(hora);
-        // console.log(dataEHora);
     };
 
     return (
